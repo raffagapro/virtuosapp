83534bd2fd10bd7843730c61db9a9a49898b438a
raffagapro <raffagapro@gmail.com>
2021-08-29 23:49:01 -0500

    change guardian to tutor, disable btns

--- app/Http/Controllers/Admin/TeacherListController.php
+++ app/Http/Controllers/Admin/TeacherListController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Models\User;
-
+use App\Models\Role;
+use App\Models\Area;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class TeacherListController extends Controller
 {
@@ -41,7 +44,27 @@ class TeacherListController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $workingPW = explode(' ', strtolower($request->nombre));
+        $pw = 'VI'.ucfirst($workingPW[0]).'2022';
+        // dd($pw, $request->all(), strtolower($request->nombre));
+        $teacher = User::create([
+            'name' => $request->nombre,
+            'email' => $request->email,
+            'curp' => $request->curp,
+            'status' => 1,
+            'password' => Hash::make($pw),
+        ]);
+
+        $role = Role::where('name', 'maestro')->first();
+        $role->user()->save($teacher);
+
+        if ((int)$request->areaId > 0) {
+            $area = Area::findOrFail((int)$request->areaId);
+            $area->user()->save($teacher);
+        }
+
+        $status = 'El meastro ha sido creado exitosamente.';
+        return redirect()->route('maestros.index')->with(compact('status'));
     }
 
     /**
@@ -86,6 +109,28 @@ class TeacherListController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // EXPANDER EN EL FUNTURO PARA INCLUIR RELATED DB INFO
+        $teacher = User::findOrFail($id);
+        $teacher->delete();
+        $status = 'El maestro ha sido eliminado exitosamente.';
+        return redirect()->route('maestros.index')->with(compact('status'));
+    }
+
+    public function activate($id)
+    {
+        $teacher = User::findOrFail($id);
+        $teacher->status = 1;
+        $teacher->save();
+        $status = 'El maestro ha sido actiavado exitosamente.';
+        return redirect()->route('maestros.index')->with(compact('status'));
+    }
+
+    public function deactivate($id)
+    {
+        $teacher = User::findOrFail($id);
+        $teacher->status = 0;
+        $teacher->save();
+        $status = 'El mastro ha sido desactivado exitosamente.';
+        return redirect()->route('maestros.index')->with(compact('status'));
     }
 }
