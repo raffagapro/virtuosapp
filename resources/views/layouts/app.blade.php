d9a7a715dd57fe52d05bf9f90640e9589ff5e60b
raffagapro <raffagapro@gmail.com>
2021-09-02 18:49:53 -0500

    aaset change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bgVirtuos shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('assets\images\nav_brand.png') }}" alt="">
+                    <img src="{{ secure_asset('assets\images\nav_brand.png') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
