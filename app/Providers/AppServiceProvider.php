6e5231f2f70e7ca6266801db5ccbc0c22b85330a
raffagapro <raffagapro@gmail.com>
2021-09-02 19:40:22 -0500

    fixed scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         if(env('APP_ENV') !== 'local')
         {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
